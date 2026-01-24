9d8e21371bc54b6653a8ce142e5954e6ea3a4392
stephen-lu <stephen.lu20@gmail.com>
2026-01-23 19:20:37 -0500

    longest substring no repeats, added optimal solution to study

--- patterns/sliding-window/examples/LongestSubstring.java
+++ patterns/sliding-window/examples/LongestSubstring.java
@@ -23,12 +23,15 @@
 // 0 <= s.length <= 5 * 104
 // s consists of English letters, digits, symbols and spaces.
 
+import java.util.HashMap;
+
 public class LongestSubstring  {
 
     public static void main (String[] args) {
         System.out.println(lengthOfLongestSubstring("abcabcbb"));
     }
 
+    // My first solution
     public static int lengthOfLongestSubstring(String s) {
         int longest = 0;
         String subString = "";
@@ -48,5 +51,22 @@ public class LongestSubstring  {
             subString = "";
         }
         return longest;
+    }
+
+    // optimal
+
+    public static int optimalSolution(String s) {
+        HashMap<Character,Integer> mpp=new HashMap<>();
+        int l=0,r=0;
+        int maxLen=0;
+        while(r<s.length()){
+            if(mpp.containsKey(s.charAt(r))) {
+                l=Math.max(l,mpp.get(s.charAt(r))+1);
+            }
+            mpp.put(s.charAt(r),r);
+            maxLen=Math.max(maxLen,r-l+1);
+            r++;
+        }
+        return maxLen;
     }
 }
